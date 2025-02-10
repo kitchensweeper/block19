f432ada6d632dcd7ea0c148a995ee6a439eeaad0
kitchensweeper <youngseo89@gmail.com>
2025-02-10 18:31:59 -0500

    feature2

--- freelancer.js
+++ freelancer.js
@@ -10,9 +10,6 @@ const names = [
   "Brian",
 ];
 
-const occupations = ["Writer", "Teacher", "Programmer", "Gardener", "Driver"];
-const maxFreelancers = 25;
-
 //freelancer array
 const freelancers = [
   { name: "Dr. Slice", price: 25, occupation: "gardener" },
@@ -24,6 +21,10 @@ const freelancers = [
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
   { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
+
+const occupations = ["Writer", "Teacher", "Programmer", "Gardener", "Driver"];
+const maxFreelancers = 25;
+const lastRenderedIndex = freelancers[freelancers.length - 1];
 
 //Random generator
 function addFreelancers() {
@@ -38,22 +39,20 @@ function addFreelancers() {
 //Average Price
 function getAveragePrice() {
   const sum = freelancers.reduce((acc, current) => acc + current.price, 0);
-  averagePrice = sum / freelancers.length;
+  return sum / freelancers.length;
 }
 
 const header = document.querySelector("header");
+const h1 = document.createElement("h1");
+h1.innerText = "Freelancer Forum";
+header.append(h1);
 const table = document.querySelector("table");
+const tableBody = document.createElement("tbody");
+table.append(tableBody);
 
 // Render the initial freelancer data
 function renderFreelancers() {
-  const h1 = document.createElement("h1");
-  h1.innerText = "Freelancer Forum";
-  header.append(h1);
-
-  const tableBody = document.createElement("tbody");
-  table.append(tableBody);
-
-  const mappedData = freelancers.map((freelancer) => {
+  const $freelancers = freelancers.map((freelancer) => {
     const tableRow = document.createElement("tr");
     const name = document.createElement("td");
     name.textContent = freelancer.name;
@@ -61,39 +60,30 @@ function renderFreelancers() {
     price.textContent = "$" + freelancer.price;
     const occupation = document.createElement("td");
     occupation.textContent = freelancer.occupation;
-
     tableRow.append(name, price, occupation);
-
-    return tableRow;
   });
 
-  tableBody.replaceChildren(...mappedData);
+  tableBody.replaceChildren(...$freelancers);
 }
 
-function renderInitialArray() {
-  for (let i = 0; i < 8; i++) {
-    const tableRow = document.createElement("tr");
-    const name = document.createElement("td");
-    name.textContent = element.name;
-    const price = document.createElement("td");
-    price.textContent = "$" + element.price;
-    const occupation = document.createElement("td");
-    occupation.textContent = element.occupation;
+function renderLast(lastRenderedIndex) {
+  const $lastFreelancer = document.createElement("tr");
+  const name = document.createElement("td");
+  name.textContent = lastRenderedIndex.name;
+  const price = document.createElement("td");
+  price.textContent = "$" + lastRenderedIndex.price;
+  const occupation = document.createElement("td");
+  occupation.textContent = lastRenderedIndex.occupation;
 
-    tableRow.append(name, price, occupation);
-
-    return tableRow;
-  }
-  tableBody.replaceChildren(tableRow);
+  $lastFreelancer.append(name, price, occupation);
+  tableBody.append($lastFreelancer);
 }
 
-function renderAverageText() {
+function renderAveragePrice() {
   const h2 = document.createElement("h2");
-  h2.innerText = `The average starting price is: `;
+  h2.innerText = `The average starting price is: $${getAveragePrice()} `;
   header.append(h2);
 }
-
-lastRenderedIndex = freelancers.length;
 
 // function renderAveragePrice() {
 //   const h2 = document.createElement("h2");
@@ -107,15 +97,13 @@ function render() {
 }
 
 // initial render
-getAveragePrice();
 render();
 
 // Add a freelancer every second until max is reached
 const freelancerInterval = setInterval(function () {
   addFreelancers();
+  renderLast(lastRenderedIndex);
   getAveragePrice();
-  renderLast();
-
   if (freelancers.length >= maxFreelancers) {
     clearInterval(freelancerInterval);
   }
